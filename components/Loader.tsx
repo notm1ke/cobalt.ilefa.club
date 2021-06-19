aaaf863de7da201f6718c315cc02dfb98e8ecaaf
Mike M <mikester70387@gmail.com>
2021-06-18 23:40:03 -0400

    Fixed loader component styling

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import PageLoader from 'react-loader-spinner';
+import styles from './styling/loader.module.css';
 
 export const Loader = () => {
-    let style = {
-        display: "flex",
-        top: "45%",
-        left: "49%",
-        position: "fixed",
-        backgroundColor: "#2C2C2C"
-    };
 
     return (
         <body className="bg-ilefa-dark">
-            <div className="loader-bg">
-                <PageLoader type="TailSpin" color="#ffffff" height={60} width={60} style={style} />
+            <div className={`loader-bg ${styles.loader}`}>
+                <PageLoader type="TailSpin" color="#ffffff" height={60} width={60} />
             </div>
         </body>
     );
---
+++ pages/loader.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../components/styling/info.module.css';
+import globalStyles from '../components/styling/home.module.css';
+
+import { Loader } from '../components';
+
+const InfoPage = () => {
+    return (
+        <Loader />
+    );
+}
+
+export default InfoPage;
